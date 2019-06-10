cb1e0a4d301d34600b19550593e561e581c3aa5a
sam6321 <sam6321@live.com.au>
2019-06-10 20:29:35 +0800

    Imported assets and added basic player movement

--- Decay/Assets/Scripts/Player/PlayerMovement.cs
+++ Decay/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
+
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
     
     void Update()
@@ -17,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
             float angle = Vector2.SignedAngle(Vector2.up, direction);
             angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angle, 360 * Time.deltaTime);
             transform.eulerAngles = new Vector3(0, 0, angle);
+            rigidbody2D.AddForce(direction * 10);
         }
     }
 }
