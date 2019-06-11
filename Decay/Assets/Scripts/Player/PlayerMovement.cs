7d54757b9a710fb0e112d52bef5e8cfce5e5d99f
sam6321 <sam6321@live.com.au>
2019-06-11 19:36:29 +0800

    Improved player movement. Added general camera follow movement script to common. Added math extensions to common

--- Decay/Assets/Scripts/Player/PlayerMovement.cs
+++ Decay/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,44 @@
 ﻿using UnityEngine;
+using Common;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    private float maxRotationDiffMagnitude = 20.0f;
+
+    [SerializeField]
+    private float movementForce = 100.0f;
+
+    [SerializeField]
+    private float rotationTorqueMultiplier = 50.0f;
+
     private Rigidbody2D rigidbody2D;
+
+    private Vector2 movement;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
-        // Rotate toward movement direction
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
         Vector2 direction = movement.normalized;
         if (direction.magnitude > 0)
         {
             float angle = Vector2.SignedAngle(Vector2.up, direction);
-            angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angle, 360 * Time.deltaTime);
-            transform.eulerAngles = new Vector3(0, 0, angle);
-            rigidbody2D.AddForce(direction * 10);
+            float diff = Mathf.DeltaAngle(transform.eulerAngles.z, angle);
+            diff = MathExtensions.ClampMagnitude(diff, 0.0f, maxRotationDiffMagnitude);
+
+            rigidbody2D.AddForce(transform.up * movementForce * Time.fixedDeltaTime);
+            rigidbody2D.AddTorque(diff * rotationTorqueMultiplier * Time.fixedDeltaTime);
         }
     }
 }
