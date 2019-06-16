61a2b320629096558dd7f33b1bb956ba1d8c1f4d
Toby <toby.scantlebury@gmail.com>
2019-06-16 16:54:04 +0800

    Calculate movement based on boat structure

--- Decay/Assets/Scripts/Player/PlayerMovement.cs
+++ Decay/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,22 +3,27 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float forwardMovementForce = 100f;
+    private float forwardMovementForce = 50f;
 
-    [SerializeField]
     private float horizontalForwardMovementForce = 50f;
 
-    [SerializeField]
-    private float rotationTorqueMultiplier = 50.0f;
+    private float rotationTorqueMultiplier = 1f;
 
     private Rigidbody2D rigidbody2D;
+    private ShipStructure structure;
 
     private Vector2 movement = new Vector2();
+
+    private float bowForce = 1.5f;
+    private float oarForce = 1.05f;
+    private float sternRotationMultiplier = 1.5f;
+    private float massMultiplier = 0.25f;
+    private float angularDragMultiplier = 5f;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        structure = GetComponent<ShipStructure>();
     }
 
     void Update()
@@ -28,20 +33,43 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
+        bool hasStern = structure.Stern != null;
+        bool hasBow = structure.Bow != null;
+        int numOars = structure.Oars.Count;
+        int leftOars = numOars / 2;
+        int rightOars = numOars / 2;
+        int width = structure.Width;
+        int numPlanks = structure.Planks.Count;
+        rigidbody2D.mass = numPlanks * massMultiplier;
+        rigidbody2D.angularDrag = numPlanks * angularDragMultiplier;
 
-        if(movement.y != 0)
+        if(numOars % 2 == 1)
         {
-            float force = movement.y < 0 ? movement.y * 0.2f : movement.y;
-            rigidbody2D.AddForce(transform.up * forwardMovementForce * force * Time.fixedDeltaTime);
+            leftOars++;
         }
-        else if(movement.x != 0)
+
+        float leftForce = movement.y * forwardMovementForce;
+        float rightForce = movement.y * forwardMovementForce;
+
+        leftForce *= hasBow ? bowForce : 1f;
+        rightForce *= hasBow ? bowForce : 1f;
+
+        leftForce *= (leftOars + 1f) * oarForce;
+        rightForce *= (rightOars + 1f) * oarForce;
+
+        rigidbody2D.AddForce(transform.up * (leftForce + rightForce) * Time.fixedDeltaTime);
+
+        if(!hasStern)
         {
-            rigidbody2D.AddForce(transform.up * horizontalForwardMovementForce * Mathf.Abs(movement.x) * Time.fixedDeltaTime);
+            rigidbody2D.AddTorque((rightForce - leftForce) * width / 2f * rotationTorqueMultiplier * Time.fixedDeltaTime);
         }
-        
-        if(movement.x != 0)
-        {
-            rigidbody2D.AddTorque(-movement.x * rotationTorqueMultiplier * Time.fixedDeltaTime);
-        }
+
+        leftForce *= movement.x;
+        rightForce *= -movement.x;
+
+        float leftTorque = -leftForce * width / 2f;
+        float rightTorque = rightForce * width / 2f;
+
+        rigidbody2D.AddTorque((rightTorque + leftTorque) * rotationTorqueMultiplier * (hasStern ? sternRotationMultiplier : 1f) * Time.fixedDeltaTime);
     }
 }
