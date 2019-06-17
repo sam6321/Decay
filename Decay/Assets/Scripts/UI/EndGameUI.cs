174f71e68e7afdc6e01480a2df431fe41ee8cd22
sam6321 <sam6321@live.com.au>
2019-06-17 21:04:10 +0800

    Added basic end game UI

---
+++ Decay/Assets/Scripts/UI/EndGameUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameUI : MonoBehaviour
+{
+    public void OnPlayAgain()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnQuit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
