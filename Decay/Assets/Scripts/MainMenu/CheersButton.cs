31d6335161043a0e7efdc07ebaafce5d1b651860
Toby <toby.scantlebury@gmail.com>
2019-06-19 21:00:37 +0800

    Finish main menu

---
+++ Decay/Assets/Scripts/MainMenu/CheersButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CheersButton : MenuButton
+{
+    [SerializeField]
+    private GameObject attributions;
+
+    [SerializeField]
+    AudioClip mouseDownAudio;
+
+    void OnMouseDown()
+    {
+        audioSource.PlayOneShot(mouseDownAudio);
+        attributions.SetActive(!attributions.activeSelf);
+    }
+}
---
+++ Decay/Assets/Scripts/MainMenu/MenuButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField]
+    Sprite offSprite;
+
+    [SerializeField]
+    Sprite onSprite;
+
+    [SerializeField]
+    AudioClip mouseOverAudio;
+
+    private SpriteRenderer spriteRenderer;
+    protected AudioSource audioSource;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnMouseOver()
+    {
+        if(spriteRenderer.sprite != onSprite)
+        {
+            audioSource.PlayOneShot(mouseOverAudio);
+            spriteRenderer.sprite = onSprite;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if(spriteRenderer.sprite != offSprite)
+        {
+            spriteRenderer.sprite = offSprite;
+        }
+    }
+}
